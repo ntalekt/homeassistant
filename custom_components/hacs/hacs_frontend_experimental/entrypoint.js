0cbea89f9dd5e01a7377bb9afeaa7cd708e2fac8
ntalekt <ntalekt@users.noreply.github.com>
2022-12-13 21:45:45 -0700

    hacs update

--- custom_components/hacs/hacs_frontend_experimental/entrypoint.js
+++ custom_components/hacs/hacs_frontend_experimental/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-22e4648c.js')")();
+  new Function("import('/hacsfiles/frontend/main-c66a9a06.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-22e4648c.js';
+  el.src = '/hacsfiles/frontend/main-c66a9a06.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
