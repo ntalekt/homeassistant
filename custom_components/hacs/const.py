55609036054e47b1cbc2e6265bedcd5f4dab2de5
ntalekt <ntalekt@users.noreply.github.com>
2023-01-28 06:52:53 -0700

    hacs update

--- custom_components/hacs/const.py
+++ custom_components/hacs/const.py
@@ -6,7 +6,9 @@ from aiogithubapi.common.const import ACCEPT_HEADERS
 NAME_SHORT = "HACS"
 DOMAIN = "hacs"
 CLIENT_ID = "395a8e669c5de9f7c6e8"
-MINIMUM_HA_VERSION = "2022.10.0"
+MINIMUM_HA_VERSION = "2022.11.0"
+
+URL_BASE = "/hacsfiles"
 
 TV = TypeVar("TV")
 
@@ -14,6 +16,8 @@ PACKAGE_NAME = "custom_components.hacs"
 
 DEFAULT_CONCURRENT_TASKS = 15
 DEFAULT_CONCURRENT_BACKOFF_TIME = 1
+
+HACS_REPOSITORY_ID = "172733314"
 
 HACS_ACTION_GITHUB_API_HEADERS = {
     "User-Agent": "HACS/action",
